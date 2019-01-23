69a5b0a68a637ae4daff773f3419ee5c66dffbb4
Bjorn Bennett <contact@bennettsdesign.com>
2019-01-23 00:52:12 -0600

    edit app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+let firstName = "Bjorn",
+    lastName = "Bennett";
+
+const fullName = "Bjorn Bennett";
+
+var person = {
+    age: "29",
+    dob: "01/01/01"
+};
+
+console.log( person.dob );
