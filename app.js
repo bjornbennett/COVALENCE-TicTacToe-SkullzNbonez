7a62f938a60b981fdc08618f704f3a6d0b908ee9
Bjorn Bennett <contact@bennettsdesign.com>
2019-01-23 01:10:50 -0600

    app.js edits

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+// variables
 let firstName = "Bjorn",
     lastName = "Bennett";
-
+// const, modern var, unchanging
 const fullName = "Bjorn Bennett";
-
-var person = {
+// let, modern var. var is function scoped, and let is block scoped
+let person = {
     age: "29",
     dob: "01/01/01"
 };
+// array
+let listStuff = [ 'yes', 'no', 'maybe'];
 
-console.log( person.dob );
+console.log( typeof fullName );
