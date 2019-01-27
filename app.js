aa27ebbbb5a231e58bf5c9b91192ccdc9219354a
Bjorn Bennett <contact@bennettsdesign.com>
2019-01-27 00:51:37 -0600

    edited app.js

--- app.js
+++ app.js
@@ -16,12 +16,41 @@ let personTemplate = {
     lastName: "Brown",
     age: 20
 };
+// .keys tells the key part of key/value pairs
+console.log( '.keys returns all keys in an object: ' + Object.keys(personTemplate) );
 
 let val1 = 15,
     val2 = 22;
-
 // x += y is shorthand for x = x + y.
 console.log(val1 += val2);
 
-// .keys tells the key part of key/value pairs
-console.log( Object.keys(personTemplate) );
+// destructuring is a JS expression to unpack values from arrays into distinct variables
+let a, b, c;
+[a,b,c] = [20,100,60];
+console.log(a,b,c);
+
+function parseProtocol(url) { 
+    var parsedURL = /^(\w+)\:\/\/([^\/]+)\/(.*)$/.exec(url);
+    if (!parsedURL) {
+      return false;
+    }
+    console.log(parsedURL); // ["https://developer.mozilla.org/en-US/Web/JavaScript", "https", "developer.mozilla.org", "en-US/Web/JavaScript"]
+  
+    var [, protocol, fullhost, fullpath] = parsedURL;
+    return protocol;
+  }
+  console.log(parseProtocol('https://developer.mozilla.org/en-US/Web/JavaScript')); // "https"
+
+// shorthand ternary expression
+let value = 6;
+value == 5 ? console.log('yes it is true') : console.log('no, it is false');
+
+// set array
+let friends = ['first','second','third','fourth','fifth'];
+// WHILE LOOP: keep looping while the count variable is less that the length of friends array
+let count = 0;
+while( count < friends.length ){
+    console.log('friends array: #'+friends[count]);
+    count++;
+}
+
