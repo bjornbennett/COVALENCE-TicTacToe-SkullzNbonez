f40da0009035deb70ba40ce7d74c2f1fc2ab9e4c
Bjorn Bennett <contact@bennettsdesign.com>
2019-01-30 21:58:53 -0600

    fixed draw code

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// gather all squares into array
+// ********* SET VARIABLES: gather all squares into array
 let cells = document.querySelectorAll('.row div'),
     clickCount = 0,
     checkBoardClass = [
@@ -14,67 +14,76 @@ let cells = document.querySelectorAll('.row div'),
     img1 = '<img src="images/bones.svg">',
     img2 = '<img src="images/skull.svg">';
 
-// listen to all squares
+// ********* MAIN EVENT: listen to all squares for clicks. will check state of game to determine what to do next
 cells.forEach(cell => {
   cell.addEventListener('click', clickedCell);
 });
 
-// function to check squares
+// ******* FUNCTION: to check squares
 function clickedCell(e) {
     console.log('clickCount: ' + clickCount);
     
-    // if 9th click, clear board, or else carry on
-    if( elSelector('.message').length > 0 ){
+    // if message has content, reset the board.
+    if( elSelector('.message').length > 0 ){ 
       resetToStart();
-    } else if(clickCount >= 9){
+    } else if(clickCount >= 9){ // if it's 9th click, clear board, or else carry on
       resetToStart();
     } else {
       
-      // if cell is empty, add X or O
-      if( e.target.innerHTML.length == 0){
-        // increase click count
-        clickCount++;
+      if( e.target.innerHTML.length == 0){// if cell is empty, add X or O
+        clickCount++; // increase click count
 
         // check if even or odd click, add X or O
-        if(clickCount % 2 == true){
+        if(clickCount % 2 == true){ // oddNumber % 2 will return true for odd, so it is odd number
           e.target.innerHTML = img1;
-        } else if(clickCount % 2 == false){
+        } else if(clickCount % 2 == false){ // evenNumber % 2 will return false for odd, so it is an even number
           e.target.innerHTML = img2;
         }
         // check if we have a winner
         checkWinningCombo(img1);
         checkWinningCombo(img2);
-
       }
     }
+    // remove the shudder class from the board. css3 animation shake on reset. iteration count 1
     if(clickCount >= 3){
       document.querySelector('#board').classList.remove('shudder');
     }
 }
 
-// check cells if a winner
-function checkWinningCombo(letter){
-  if(clickCount > 8){
-    cells.forEach(cell => {
-      let msg = 'Arrrrh arrrrrh! <strong>DRAW!</strong><br><span>click the board to start another game</span>';
-      document.querySelector('.message').innerHTML = msg;
-    });
-  }
+// ******** FUNCTION: check cells for a winner
+function checkWinningCombo(content){
   for (let i = 0; i < checkBoardClass.length; i++) {
     [k,l,m] = [checkBoardClass[i][0],checkBoardClass[i][1],checkBoardClass[i][2]];
     
     // if winner!
-    if( elSelector(k) == letter  && elSelector(l) == letter && elSelector(m) == letter ){
+    if( elSelector(k) == content  && elSelector(l) == content && elSelector(m) == content ){
       let msg = 'Arrrrh arrrrrh! <strong>Looky like we \'ava champ!!</strong><br> <span>click the board to start another game</span>';
       document.querySelector('.message').innerHTML = msg;
       document.querySelector(k).classList.add('cream1');
       document.querySelector(l).classList.add('cream2');
       document.querySelector(m).classList.add('cream3');
+      // listen to all squares
+      cells.forEach(cell => {
+        cell.classList.add('blood');
+      });
+      console.log('winner winner, chicken dinner');
     }
+  }
+
+  // if draw
+  if(clickCount == 9 && document.querySelector('.row div').classList.contains('cream1') === 0 ){
+    let msg = 'Arrrrh arrrrrh! <strong>DRAW!</strong><br><span>click the board to start another game</span>';
+    document.querySelector('.message').classList = msg;
+    console.log('clickcount 9??');
+    console.log('draw draw, we iz poor');
+    // document.querySelector('.inner-board').classList.add('dodge');
+    cells.forEach(cell => {
+      cell.classList.add('blood');
+    });
   }
 }
 
-// reset game
+// ******* FUNCTION: reset / zero-set the game
 function resetToStart(){
   cells.forEach(cell => {
     cell.innerHTML = "";
@@ -82,9 +91,10 @@ function resetToStart(){
   clickCount = 0;
   document.querySelector('.message').innerHTML = "";
   cells.forEach(cell => {
-    cell.classList.remove('cream1', 'cream2', 'cream3');
+    cell.classList.remove('cream1', 'cream2', 'cream3', 'blood');
   });
   document.querySelector('#board').classList.add('shudder');
+  document.querySelector('.inner-board').classList.remove('dodge');
   console.log('Game has been reset!');
 }
 
